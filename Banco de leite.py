03c0704d091abe998f806387e64913b7bed0aad4
Gabriel Bremenkamp <39781077+Gabrielbs182@users.noreply.github.com>
2018-12-18 21:22:32 -0200

    Update Banco de leite.py

--- Banco de leite.py
+++ Banco de leite.py
@@ -29,7 +29,7 @@ def send_start(msg):
 def texto(msg):
   texto = msg.text
   chat_id = msg.chat.id
-  if chat_id == <chat_id_responsavel> and texto == '5':
+  if chat_id == 584039241 and texto == '5':
 
     conn = sqlite3.connect('leite.db')
     cursor = conn.cursor()
@@ -42,14 +42,20 @@ def texto(msg):
       print(i[2])
       print(data)
       if i[2] == data:
-        msg1 = "Bom dia, {} <mensagem>".format(i[1])
+        msg1 = "Prezada {}, o banco de leite encontra-se com abastecimento prejudicado.\nPrecisamos de sua ajuda para manter a alimentação de recém nascidos das UTI’s.\nCompareça ao banco de leite humano mais proximo para contribuir com essa causa, para localização e informações sobre bancos de leites proximos digite '2'".format(i[1])
       chat_id = i[0]
       bot.send_message( chat_id , msg1 )
 
   elif texto == '1':
 	  bot.send_message( msg.chat.id , "Texto de informações" )
   elif texto == '2':
-    bot.send_message( msg.chat.id , "localização dos bancos de leite mais proximos" )
+    bot.send_message( msg.chat.id , "Hospital Estadual Infantil e Maternidade de Vila Velha\nHorário de funcionamento: 08 às 17 horas, de segunda a sexta-feira. \nEndereço: Avenida Ministro Salgado Filho, 918, Soteco, Vila Velha.\nContato: 3636-3151")
+    bot.send_message( msg.chat.id , "Hospital Estadual Dório Silva\nHorário de funcionamento: 08 às 17 horas, de segunda a sexta-feira.\nEndereço: Avenida Eudes Scherrer de Souza, s/n, Laranjeiras, Serra.\nContato: 3138-8905")
+    bot.send_message( msg.chat.id , "Hospital da Polícia Militar do Espírito Santo (HPM)\nEndereço: Bento Ferreira, em Vitória.\nContato: 3636-6568")
+    bot.send_message( msg.chat.id , "Hospital das Clínicas (referência do Estado)\nHorário de funcionamento: 08 às 21 horas, de segunda a sexta-feira.\nEndereço: Avenida Marechal Campos, s.nº, Maruípe.\nContato: 3335-7424 – 3335-7377")
+    bot.send_message( msg.chat.id , "Santa Casa de Misericórdia\nHorário de funcionamento:07 às 16 horas, de segunda a sexta-feira.\nEndereço: Rua Dr. João dos Santos Neves, 143, Vila Rubim, Vitória.\nContato: 3212-7246")
+    bot.send_message( msg.chat.id , "Hospital Evangélico de Cachoeiro de Itapemirim\nHorário de funcionamento: 07 às 17 horas, de segunda a sexta-feira.\nEndereço: Rua Anacleto Ramos, 55, Bairro Ferroviários.\nContato: (28) 3521-7045")
+    bot.send_message( msg.chat.id , "Hospital e Maternidade São José, Colatina\nHorário de funcionamento: das 07 às 17h20, de segunda a sexta-feira.\nEndereço: Ladeira Cristo Rei, Centro de Colatina.\nContato: (27) 2102-2100")
   else:
     bot.send_message( msg.chat.id , "Infelizmente não tenho nenhuma opção para a sua resposta, por favor tente uma outra resposta." )
 
